3a32392ad56f4e3bff64c7502193dcdd117f285c
kalyan6013 <kalyan6013@gmail.com>
2020-03-25 22:40:58 +0530

    changes in script files

--- zkp/scripts/script.sh
+++ zkp/scripts/script.sh
@@ -23,13 +23,13 @@ LANGUAGE=`echo "$LANGUAGE" | tr [:upper:] [:lower:]`
 COUNTER=1
 MAX_RETRY=10
 
-CC_SRC_PATH="github.com/chaincode/chaincode_example02/go/"
+CC_SRC_PATH="github.com/chaincode/zkp/go/"
 if [ "$LANGUAGE" = "node" ]; then
-	CC_SRC_PATH="/opt/gopath/src/github.com/chaincode/chaincode_example02/node/"
+	CC_SRC_PATH="/opt/gopath/src/github.com/chaincode/zkp/node/"
 fi
 
 if [ "$LANGUAGE" = "java" ]; then
-	CC_SRC_PATH="/opt/gopath/src/github.com/chaincode/chaincode_example02/java/"
+	CC_SRC_PATH="/opt/gopath/src/github.com/chaincode/zkp/java/"
 fi
 
 echo "Channel name : "$CHANNEL_NAME
@@ -89,7 +89,7 @@ echo "Installing chaincode on peer0.org1..."
 installChaincode 0 1
 echo "Install chaincode on peer0.org2..."
 installChaincode 0 2
-eecho "Install chaincode on peer0.org3..."
+echo "Install chaincode on peer0.org3..."
 installChaincode 0 3
 
 # Instantiate chaincode on peer0.org2
@@ -97,26 +97,26 @@ echo "Instantiating chaincode on peer0.org2..."
 instantiateChaincode 0 2
 
 # Query chaincode on peer0.org1
-echo "Querying chaincode on peer0.org1..."
-chaincodeQuery 0 1 100
+# echo "Querying chaincode on peer0.org1..."
+# chaincodeQuery 0 1 100
 
 # Invoke chaincode on peer0.org1 and peer0.org2
-echo "Sending invoke transaction on peer0.org1 peer0.org2..."
-chaincodeInvoke 0 1 0 2
+# echo "Sending invoke transaction on peer0.org1 peer0.org2..."
+# chaincodeInvoke 0 1 0 2
 
 ## Install chaincode on peer1.org2
-echo "Installing chaincode on peer1.org2..."
-installChaincode 1 2
+# echo "Installing chaincode on peer1.org2..."
+# installChaincode 1 2
 
 # Query on chaincode on peer1.org2, check if the result is 90
-echo "Querying chaincode on peer1.org2..."
-chaincodeQuery 1 2 90
+# echo "Querying chaincode on peer1.org2..."
+# chaincodeQuery 1 2 90
 
-echo "Installing chaincode on peer1.org3..."
-installChaincode 1 3
+# echo "Installing chaincode on peer1.org3..."
+# installChaincode 1 3
 
-echo "Querying chaincode on peer1.org3..."
-chaincodeQuery 1 3 90
+# echo "Querying chaincode on peer1.org3..."
+# chaincodeQuery 1 3 90
 
 echo
 echo "========= All GOOD, BYFN execution completed =========== "
@@ -129,5 +129,43 @@ echo "|  _|   |  \| | | | | | "
 echo "| |___  | |\  | | |_| | "
 echo "|_____| |_| \_| |____/  "
 echo
+
+echo "You need to manually instantiate, invoke the transactions for initData & queryData for now"
+echo "Commands are below"
+
+echo "==============================================================================================="
+
+echo "peer chaincode instantiate -o orderer.example.com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C mychannel -n mycc8 -l node -v 1.0 -c '{"Args":["init"]}' -P " OR ('Org1MSP.peer','Org2MSP.peer', 'Org3MSP.peer')" --collections-config /opt/gopath/src/github.com/chaincode/zkp/zkp.json"
+echo "==============================================================================================="
+
+echo "peer chaincode invoke -o orderer.example.com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C mychannel -n mycc -c '{"Args":["initData","0001","1234"]}'"
+echo "==============================================================================================="
+
+echo "export this environmental variables"
+echo "==============================================================================================="
+
+echo " export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=peer0.org1.example.com:7051
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+"
+echo "==============================================================================================="
+
+echo "peer chaincode invoke -o orderer.example.com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C mychannel -n mycc -c '{"Args":["queryData", "0001","1234"]}'"
+echo "==============================================================================================="
+
+echo "When you access from Org2/Org3 data will not available"
+
+echo "==============================================================================================="
+
+echo "export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+export CORE_PEER_ADDRESS=peer0.org2.example.com:7051
+export CORE_PEER_LOCALMSPID="Org2MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+"
+echo "==============================================================================================="
+
+echo "peer chaincode invoke -o orderer.example.com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C mychannel -n mycc -c '{"Args":["queryData", "0001","1234"]}'"
+echo "==============================================================================================="
 
 exit 0
