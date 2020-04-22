a55ce77b344e7b57c18c08423db7e8c3c1841808
kalyan6013 <kalyan6013@gmail.com>
2020-04-22 19:26:59 +0530

    cleaning errors in the JS files

--- chaincode/PublicParameters.js
+++ chaincode/PublicParameters.js
@@ -4,41 +4,55 @@
 import {alt_bn128} from "./alt_bn128.js";
 import {Conversion} from "./Conversion.js";
 
-contract 
+// contract 
 function PublicParameters() {
 
     event VectorCreated(uint256 _i);
 
-    using alt_bn128 for uint256;
-    using alt_bn128 for alt_bn128.G1Point;
-    using Conversion for uint256;
+    // using alt_bn128 for uint256;
+    let alt_bn128, Conversion;
+    // using alt_bn128 for alt_bn128.G1Point;
+    let alt_bn128.G1Point;
+    // using Conversion for uint256;
 
-    uint256 public constant m = 64;
-    uint256 public constant n = 6;
 
-    uint256[2] public signatureGenerator;
-    uint256[2] public baseG;
-    uint256[2] public baseH;
+    // uint256 public constant m = 64;
+    // uint256 public constant n = 6;
+    const m = 64;
+    const n = 6;
+
+    // uint256[2] public signatureGenerator;
+    let signatureGenerator = [2];
+    let baseG=[2]
+    let baseH=[2]
+    // uint256[2] public baseG;
+    // uint256[2] public baseH;
     uint256[m*2] public gVector;
     uint256[m*2] public hVector;
     
-    uint256 public lastGcreated = 0;
-    uint256 public lastHcreated = 0;
-
-    constructor() public {
+    // uint256 public lastGcreated = 0;
+    // uint256 public lastHcreated = 0;
+    let lastGcreated, lastHcreated=0
+    constructor() 
+    // public
+     {
         signatureGenerator = signaturePublicGenerator();
         baseG = peddersenBaseG();
         baseH = peddersenBaseH();
     }
 
-    function createGVector() public returns (bool success) {
+    function createGVector()
+    //  public returns (bool success) 
+     {
         require(lastGcreated < m);
-        uint256 maxToCreate = m - lastGcreated;
+        // uint256 
+        let maxToCreate = m - lastGcreated;
         if (maxToCreate > n) {
             maxToCreate = n;
         }
-        uint256[2] memory reusablePoints;
-        for (uint256 i = lastGcreated; i < lastGcreated+maxToCreate; i++) {
+        // uint256[2] memory reusablePoints;
+        let reusablePoints=[2]
+        for ( i = lastGcreated; i < lastGcreated+maxToCreate; i++) {
             reusablePoints = getGVectorComponent(i);
             gVector[i*2] = reusablePoints[0];
             gVector[i*2 + 1] = reusablePoints[1];
@@ -48,14 +62,18 @@ function PublicParameters() {
         return true;
     }
 
-    function createHVector() public returns (bool success) {
+    function createHVector() 
+    // public returns (bool success) 
+    {
         require(lastHcreated < m);
-        uint256 maxToCreate = m - lastHcreated;
+        // uint256 
+        let maxToCreate = m - lastHcreated;
         if (maxToCreate > n) {
             maxToCreate = n;
         }
-        uint256[2] memory reusablePoints;
-        for (uint256 i = lastHcreated; i < lastHcreated+maxToCreate; i++) {
+        // uint256[2] memory reusablePoints;
+        reusablePoints=[2];
+        for ( i = lastHcreated; i < lastHcreated+maxToCreate; i++) {
             reusablePoints = getHVectorComponent(i);
             hVector[i*2] = reusablePoints[0];
             hVector[i*2 + 1] = reusablePoints[1];
@@ -64,72 +82,110 @@ function PublicParameters() {
         return true;
     }
 
-    function signaturePublicGenerator() public pure returns (uint256[2] point) {
+    function signaturePublicGenerator() 
+    // public pure returns (uint256[2] point) 
+    {
         return [uint256(1), uint256(2)];
     }
 
-    function peddersenBaseG() public view returns (uint256[2] point) {
-        bytes32 h = keccak256("G");
+    function peddersenBaseG() 
+    // public view returns (uint256[2] point) 
+    {
+        // bytes32 
+        let h = keccak256("G");
         alt_bn128.G1Point memory g1p = alt_bn128.uintToCurvePoint(uint256(h));
         return [g1p.X, g1p.Y];
     }
     
-    function peddersenBaseH() public view returns (uint256[2] point) {
-        bytes32 h = keccak256("H");
+    function peddersenBaseH() 
+    // public view returns (uint256[2] point) 
+    {
+        // bytes32
+        let h = keccak256("H");
         alt_bn128.G1Point memory g1p = alt_bn128.uintToCurvePoint(uint256(h));
         return [g1p.X, g1p.Y];
     }
 
-    function getGVectorComponent(uint256 index) public view returns (uint256[2] point) {
+    function getGVectorComponent( index) 
+    // public view returns (uint256[2] point) 
+    {
         require(index < m);
-        bytes32 h = keccak256("G", index.uintToBytes());
+        // bytes32
+        let  h = keccak256("G", index.uintToBytes());
         alt_bn128.G1Point memory g1p = alt_bn128.uintToCurvePoint(uint256(h));
         return [g1p.X, g1p.Y];
     }
 
-    function getHVectorComponent(uint256 index) public view returns (uint256[2] point) {
+    function getHVectorComponent(
+        // uint256
+         index) 
+        //  public view returns (uint256[2] point) 
+         {
         require(index < m);
-        bytes32 h = keccak256("H", index.uintToBytes());
+        // bytes32 
+        let h = keccak256("H", index.uintToBytes());
         alt_bn128.G1Point memory g1p = alt_bn128.uintToCurvePoint(uint256(h));
         return [g1p.X, g1p.Y];
     }
     
-    function getGVector() public view returns (uint256[2*m] points) {
+    function getGVector() 
+    // public view returns (uint256[2*m] points) 
+    {
         return gVector;
     }
     
-    function getHVector() public view returns (uint256[2*m] points) {
+    function getHVector()
+    //  public view returns (uint256[2*m] points) 
+     {
         return hVector;
     }
 
-    function generator() public view returns (alt_bn128.G1Point memory p) {
+    function generator()
+    //  public view returns (alt_bn128.G1Point memory p) 
+     {
         return assemblePoint(signatureGenerator);
     }
 
-    function G() public view returns (alt_bn128.G1Point memory p) {
+    function G()
+    //  public view returns (alt_bn128.G1Point memory p) 
+     {
         return assemblePoint(baseG);
     }
 
-    function H() public view returns (alt_bn128.G1Point memory p) {
+    function H()
+    //  public view returns (alt_bn128.G1Point memory p) 
+     {
         return assemblePoint(baseH);
     }
 
-    function gs() public view returns(alt_bn128.G1Point[m] memory points){
+    function gs() 
+    // public view returns(alt_bn128.G1Point[m] memory points)
+    {
         return assemblePointsFromEncodings(gVector);
     }
 
-    function hs() public view returns(alt_bn128.G1Point[m] memory points){
+    function hs() 
+    // public view returns(alt_bn128.G1Point[m] memory points)
+    {
         return assemblePointsFromEncodings(hVector);
     }
 
-    function assemblePointsFromEncodings(uint256[m*2] _pointsEncoding) internal pure returns(alt_bn128.G1Point[m] memory points) {
-        for (uint256 i = 0; i < m; i++) {
+    function assemblePointsFromEncodings(
+        // uint256[m*2] 
+        _pointsEncoding) 
+    // internal pure returns(alt_bn128.G1Point[m] memory points) 
+    {
+        for ( i = 0; i < m; i++) {
             points[i] = alt_bn128.G1Point(_pointsEncoding[2*i], _pointsEncoding[2*i + 1]);
         }
         return points;
     }
 
-    function assemblePoint(uint256[2] _encoding) internal pure returns(alt_bn128.G1Point memory point) {
+    function assemblePoint(
+    // (uint256[2]
+         _encoding)
+        //   internal pure returns(alt_bn128.G1Point memory point)
+           {
         return alt_bn128.G1Point(_encoding[0], _encoding[1]);
     }
 }
--- chaincode/RangeProofVerifier.js
+++ chaincode/RangeProofVerifier.js
@@ -1,17 +1,21 @@
-pragma solidity ^0.4.24;
-pragma experimental ABIEncoderV2;
+// pragma solidity ^0.4.24;
+// pragma experimental ABIEncoderV2;
 
 import "./alt_bn128.sol";
 import {EfficientInnerProductVerifier} from "./EfficientInnerProductVerifier.sol";
 import {PublicParameters} from "./PublicParameters.sol";
 
-contract RangeProofVerifier {
-    using alt_bn128 for uint256;
+// contract 
+function RangeProofVerifier() {
+    let alt_bn128
+    // using alt_bn128 for uint256;
     using alt_bn128 for alt_bn128.G1Point;
-    event DebugEvent(uint256 a, uint256 b, uint256 c);
+    // event DebugEvent(uint256 a, uint256 b, uint256 c);
+    event DebugEvent(a,b,c);
 
-    uint256 public constant m = 64;
-    uint256 public constant n = 6;
+    // uint256 public constant m = 64;
+    // uint256 public constant n = 6;
+    const m=64, n=6;
 
     PublicParameters public publicParameters;
     EfficientInnerProductVerifier public ipVerifier;
@@ -20,9 +24,13 @@ contract RangeProofVerifier {
     // uint256 public lastPowerCreated = 0;
 
     constructor (
-        address _publicParameters,
-        address _ipVerifier
-    ) public {
+        // address 
+        _publicParameters,
+        // address 
+        _ipVerifier
+    )
+    //  public 
+     {
         require(_publicParameters != address(0));
         require(_ipVerifier != address(0));
         publicParameters = PublicParameters(_publicParameters);
@@ -33,37 +41,20 @@ contract RangeProofVerifier {
         require(n == ipVerifier.n());
     }
 
-    // function producePowers() public returns(bool success) {
-    //     require(lastPowerCreated < m);
-    //     uint256 base = 2;
-    //     uint256 maxToCreate = m - lastPowerCreated;
-    //     if (maxToCreate > n) {
-    //         maxToCreate = n;
-    //     }
-    //     uint256 i;
-    //     if (lastPowerCreated == 0) {
-    //         twos[0] = 1;
-    //         twos[1] = base;
-    //         for (i = 2; i < m; i++) {
-    //             twos[i] = twos[i-1].mul(base);
-    //         }
-    //     } else {
-    //         for (i = lastPowerCreated; i < lastPowerCreated + maxToCreate; i++) {
-    //             twos[i] = twos[i-1].mul(base);
-    //         }
-    //     }
-    //     lastPowerCreated = lastPowerCreated + maxToCreate;
-    //     return true;
-    // }
-
     function verify(
-        uint256[10] coords, // [input_x, input_y, A_x, A_y, S_x, S_y, commits[0]_x, commits[0]_y, commits[1]_x, commits[1]_y]
-        uint256[5] scalars, // [tauX, mu, t, a, b]
-        uint256[2*n] ls_coords, // 2 * n
-        uint256[2*n] rs_coords  // 2 * n
-    ) public 
-    // view 
-    returns (bool) {
+        // uint256[10] 
+        coords, // [input_x, input_y, A_x, A_y, S_x, S_y, commits[0]_x, commits[0]_y, commits[1]_x, commits[1]_y]
+        // uint256[5]
+         scalars, // [tauX, mu, t, a, b]
+        // uint256[2*n]
+         ls_coords, // 2 * n
+        // uint256[2*n]
+         rs_coords  // 2 * n
+    ) 
+    // public 
+    // // view 
+    // returns (bool) 
+    {
         RangeProof memory rangeProof;
         alt_bn128.G1Point memory input = alt_bn128.G1Point(coords[0], coords[1]);
         rangeProof.A = alt_bn128.G1Point(coords[2], coords[3]);
@@ -85,33 +76,38 @@ contract RangeProofVerifier {
         alt_bn128.G1Point A;
         alt_bn128.G1Point S;
         alt_bn128.G1Point[2] commits;
-        uint256 tauX;
-        uint256 mu;
-        uint256 t;
+        let tauX,mu,t;
+        // uint256 tauX;
+        // uint256 mu;
+        // uint256 t;
         InnerProductProof ipProof;
     }
 
     struct InnerProductProof {
         uint256[2*n] ls;
         uint256[2*n] rs;
-        uint256 a;
-        uint256 b;
+        let a,b;
+        // uint256 a;
+        // uint256 b;
     }
 
-    event Proof(uint256 x, uint256 y);
+    // event Proof(uint256 x, uint256 y);
+    event Proof( x,  y);
+
 
     struct Board {
-        uint256 y;
+        // uint256 yuint256;
+        let y,z,zSquared,zCubed,x,k,uChallenge;
         uint256[m] ys;
-        uint256 z;
-        uint256 zSquared;
-        uint256 zCubed;
+        // uint256 z;
+        // uint256 zSquared;
+        // uint256 zCubed;
         uint256[m] twoTimesZSquared;
-        uint256 x;
+        // uint256 x;
         alt_bn128.G1Point lhs;
-        uint256 k;
+        // uint256 k;
         alt_bn128.G1Point rhs;
-        uint256 uChallenge;
+        // uint256 uChallenge;
         alt_bn128.G1Point u;
         alt_bn128.G1Point P;
     }
@@ -119,9 +115,11 @@ contract RangeProofVerifier {
     function verifyInternal(
         alt_bn128.G1Point input,
         RangeProof proof
-    ) internal 
-    view 
-    returns (bool) {
+    ) 
+    // internal 
+    // view 
+    // returns (bool) 
+    {
         alt_bn128.G1Point memory G = publicParameters.G();
         alt_bn128.G1Point memory H = publicParameters.H();
         alt_bn128.G1Point[m] memory gs = publicParameters.gs();
@@ -154,56 +152,78 @@ contract RangeProofVerifier {
         return ipVerifier.verifyWithCustomParams(b.P, proof.ipProof.ls, proof.ipProof.rs, proof.ipProof.a, proof.ipProof.b, gs, hPrimes, b.u);
     }
 
-    function addVectors(uint256[m] a, uint256[m] b) internal pure returns (uint256[m] result) {
-        for (uint256 i = 0; i < m; i++) {
+    function addVectors(
+        // uint256[m] 
+        a, 
+        // uint256[m] 
+        b) 
+        // internal pure returns (uint256[m] result) 
+        {
+        for (
+            // uint256 
+            i = 0; i < m; i++) {
             result[i] = a[i].add(b[i]);
         }
     }
 
     function haddamard(alt_bn128.G1Point[m] ps, uint256[m] ss) internal view returns (alt_bn128.G1Point[m] result) {
-        for (uint256 i = 0; i < m; i++) {
+    function haddamard(alt_bn128.G1Point[m] ps, ss) i {
+        for (
+            // uint256
+             i = 0; i < m; i++) {
             result[i] = ps[i].mul(ss[i]);
         }
     }
 
-    // function haddamard_inv(alt_bn128.G1Point[m] ps, uint256[m] ss) internal view returns (alt_bn128.G1Point[m] result) {
-    //     for (uint256 i = 0; i < m; i++) {
-    //         result[i] = ps[i].mul(ss[i].inv());
-    //     }
-    // }
-
-    function sumScalars(uint256[m] ys) internal pure returns (uint256 result) {
-        for (uint256 i = 0; i < m; i++) {
+   
+    function sumScalars(uint256[m] ys) 
+    // internal pure returns (uint256 result) 
+    {
+        for ( i = 0; i < m; i++) {
             result = result.add(ys[i]);
         }
     }
 
-    function sumPoints(alt_bn128.G1Point[m] ps) internal view returns (alt_bn128.G1Point sum) {
+    function sumPoints(alt_bn128.G1Point[m] ps) 
+    // internal view returns (alt_bn128.G1Point sum) 
+    {
         sum = ps[0];
-        for (uint256 i = 1; i < m; i++) {
+        for ( i = 1; i < m; i++) {
             sum = sum.add(ps[i]);
         }
     }
 
-    function commit(alt_bn128.G1Point[m] ps, uint256[m] ss) internal view returns (alt_bn128.G1Point commit) {
+    function commit(alt_bn128.G1Point[m] ps, uint256[m] ss) 
+    // internal view returns (alt_bn128.G1Point commit) 
+    {
         commit = ps[0].mul(ss[0]);
-        for (uint256 i = 1; i < m; i++) {
+        for ( i = 1; i < m; i++) {
             commit = commit.add(ps[i].mul(ss[i]));
         }
     }
 
-    function powers(uint256 base) internal pure returns (uint256[m] powers) {
+    function powers(
+        // uint256
+         base)
+    //  internal pure returns (uint256[m] powers) 
+     {
         powers[0] = 1;
         powers[1] = base;
-        for (uint256 i = 2; i < m; i++) {
+        for ( i = 2; i < m; i++) {
             powers[i] = powers[i-1].mul(base);
         }
     }
 
-    function powers(uint256 base, uint256 firstPower, uint256 numberOfPowers) internal pure returns (uint256[] powers) {
-        uint256[] memory pwrs = new uint256[](numberOfPowers);
+    // function powers(uint256 base, uint256 firstPower, uint256 numberOfPowers)
+    function powers(base,firstPower,numberOfPowers)
+    //  internal pure returns (uint256[] powers) 
+     {
+        // uint256[] memory pwrs = new uint256[](numberOfPowers);
+        let pwrs=[];
+        pwrs=numberOfPowers;
         pwrs[0] = 1;
-        uint256 i = 0;
+        // uint256
+        let  i = 0;
         if (firstPower != 0) {
             for (i = 0; i < firstPower; i++) {
                 pwrs[0] = pwrs[0].mul(base);
@@ -215,8 +235,14 @@ contract RangeProofVerifier {
         return pwrs;
     }
 
-    function times(uint256[m] v, uint256 x) internal pure returns (uint256[m] result) {
-        for (uint256 i = 0; i < m; i++) {
+    function times(
+        // uint256[m] 
+        v, 
+        // uint256 
+        x) 
+    // internal pure returns (uint256[m] result) 
+    {
+        for ( i = 0; i < m; i++) {
             result[i] = v[i].mul(x);
         }
     }
